8d743f655baf70232569bd873365ccc4277c530e
elyakrrr <503268@niuitmo.ru>
2026-01-03 12:00:00 +0300

    add records in class

--- models/Inspector.java
+++ models/Inspector.java
@@ -1,5 +1,8 @@
 package models;
 
+import records.InterrogationResults;
+import records.RaidResults;
+
 import java.util.List;
 
 public class Inspector extends PoliceOfficer {
@@ -12,5 +15,27 @@ public Statuette take(Monolith monolith) {
         monolith.setStatuette(null);
         System.out.println(getName() + " забирает статуэтку с монолита.");
         return statuette;
+    }
+  
+  public RaidResults count(List<Cultist> cultists) {
+        int arrested = 0;
+        int killed = 0;
+        int wounded = 0;
+        int escaped = 0;
+        
+        for (Cultist cultist : cultists) {
+            switch (cultist.getState()) {
+                case ARRESTED -> arrested++;
+                case KILLED -> killed++;
+                case WOUNDED_ARRESTED -> wounded++;
+                case FUGITIVE -> escaped++;
+            }
+        }
+        
+        return new RaidResults(arrested, killed, wounded, escaped);
+    }
+
+  public InterrogationResults interrogate(Cultist cultist) {
+        return new InterrogationResults("Негритянский фетишизм", "Продвижение своей религии");
     }
 }
